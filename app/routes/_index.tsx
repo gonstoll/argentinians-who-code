60e2849b65a19a94ba184094662470e44465ed94
Gonzalo Stoll <gonzalostoll@gonzalo.local>
2024-01-02 18:53:13 +0100

    Initial commit

---
+++ app/routes/_index.tsx
@@ -0,0 +1,10 @@
+export function meta() {
+  return [
+    {title: 'New Remix App'},
+    {name: 'description', content: 'Welcome to Remix!'},
+  ]
+}
+
+export default function Index() {
+  return <div className="mb-0 mt-0 flex">Hello!</div>
+}
